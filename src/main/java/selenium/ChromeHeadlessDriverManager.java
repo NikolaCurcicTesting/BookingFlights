5f90aad1cd72ecfd4a2c39d174d67c9d1ff3ece4
NikolaCurcicTesting <dzonifree@yahoo.com>
2022-11-13 21:37:51 +0100

    added changed to driver manager

--- src/main/java/selenium/ChromeHeadlessDriverManager.java
+++ src/main/java/selenium/ChromeHeadlessDriverManager.java
@@ -1,2 +1,18 @@
-package selenium;public class ChromeHeadlessDriverManager {
+package selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeHeadlessDriverManager extends DriverManager {
+    @Override
+    public void createWebDriver() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--window-size=1920,1080");
+
+        driver = new ChromeDriver(options);
+    }
 }
